1d4927ac8c90b1b2045d4dc5b0b2844312528016
satshi-sh <satoss1108@gmail.com>
2023-05-29 22:14:41 +0000

    Added tls option for security

--- js/connect.js
+++ js/connect.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 const databaseURL = process.env.DATABASE_URL;
+const fs = require('fs')
+
+const options = {
+  tls: true,
+  tlsCertificateKeyFile: '/etc/ssl/mongodb.pem',
+  tlsAllowInvalidCertificates: true,
+};
 
 async function connect() {
   try {
     await mongoose.connect(databaseURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      ...options
     });
     console.log('Connected to MongoDB');
   } catch (error) {
